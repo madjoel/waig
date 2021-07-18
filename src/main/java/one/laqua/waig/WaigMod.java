4316cd89c9d87f13a54508e0aab9d444e8eb5f3a
Matthieu Laqua <mlmatthieul@gmail.com>
2021-07-18 14:57:13 +0200

    Change entrypoint to be client instead of main, Add keybinding to toggle hud (default: F6)

--- src/main/java/one/laqua/waig/WaigMod.java
+++ src/main/java/one/laqua/waig/WaigMod.java
@@ -1,13 +1,17 @@
 package one.laqua.waig;
 
-import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
 
-public class WaigMod implements ModInitializer {
+public class WaigMod implements ClientModInitializer {
 
     public static Logger LOGGER = LogManager.getLogger();
 
@@ -15,9 +19,26 @@ public class WaigMod implements ModInitializer {
     public static final String MOD_NAME = "Where Am I Going";
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         log(Level.INFO, "Initializing");
+
+        // register render callback
         HudRenderCallback.EVENT.register(CompassHud::onHudRender);
+
+        // add key binding to toggle visibility of the hud
+        KeyBinding binding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.waig.toggle",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_F6,
+                KeyBinding.UI_CATEGORY
+        ));
+
+        // add response to the keybinding
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (binding.wasPressed()) {
+                CompassHud.toggleVisibility();
+            }
+        });
     }
 
     public static void log(Level level, String message){
