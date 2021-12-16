7978e72afcab0ee0165683c51c8881bf154643df
Matthieu Laqua <mlmatthieul@gmail.com>
2021-12-16 15:13:20 +0100

    Restructure whole mod

--- src/main/java/one/laqua/waig/client/CompassHud.java
+++ src/main/java/one/laqua/waig/client/CompassHud.java
@@ -1,4 +1,4 @@
-package one.laqua.waig;
+package one.laqua.waig.client;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
---
+++ src/main/java/one/laqua/waig/client/WaigClient.java
@@ -0,0 +1,50 @@
+package one.laqua.waig.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
+
+@Environment(EnvType.CLIENT)
+public class WaigClient implements ClientModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "waig";
+    public static final String MOD_NAME = "Where Am I Going";
+
+    @Override
+    public void onInitializeClient() {
+        log(Level.INFO, "Initializing");
+
+        // register render callback
+        HudRenderCallback.EVENT.register(CompassHud::onHudRender);
+
+        // add key binding to toggle visibility of the hud
+        KeyBinding binding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.waig.toggle",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_F6,
+                KeyBinding.UI_CATEGORY
+        ));
+
+        // add response to the keybinding
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (binding.wasPressed()) {
+                CompassHud.toggleVisibility();
+            }
+        });
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+}
