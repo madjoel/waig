312989785d40dfa0942923a55fcd7506746d1e5d
Matthieu Laqua <mlmatthieul@gmail.com>
2022-01-08 16:39:34 +0100

    Update CompassHud to show only in the configurable circumstances

--- src/main/java/one/laqua/waig/client/CompassHud.java
+++ src/main/java/one/laqua/waig/client/CompassHud.java
@@ -5,7 +5,9 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import one.laqua.waig.client.config.WaigConfig;
 import one.laqua.waig.mixin.BossBarHudAccessor;
 
 @Environment(EnvType.CLIENT)
@@ -15,6 +17,8 @@ public class CompassHud {
     private static final String compass_text_triple = compass_text_simple + compass_text_simple + compass_text_simple;
     private static final int oneSideLength = 20;
 
+    private static final ItemStack compass_stack = Items.COMPASS.getDefaultStack();
+
     private static boolean visible = true;
 
     public static void onHudRender(MatrixStack matrices, float v) {
@@ -22,14 +26,28 @@ public class CompassHud {
             return;
         }
 
+        // get the player
         MinecraftClient client = MinecraftClient.getInstance();
         ClientPlayerEntity p = client.player;
         if (p == null) {
             return;
         }
 
-        if (WaigConfig.onlyShowHudWhenCompassInHand() && !p.isHolding(Items.COMPASS)) {
-            return;
+        // check the configuration
+        switch (WaigConfig.getHudShowMode()) {
+            case ALWAYS -> {
+                // nothing to check in this case, just continue
+            }
+            case INVENTORY -> {
+                if (!p.getInventory().contains(compass_stack)) {
+                    return;
+                }
+            }
+            case HAND -> {
+                if (!p.isHolding(Items.COMPASS)) {
+                    return;
+                }
+            }
         }
 
         float modYaw = (p.headYaw % 360.0f + 360.0f) % 360.0f;
