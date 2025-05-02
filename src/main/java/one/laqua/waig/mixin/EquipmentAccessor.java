4fc0a375f2deac80152db33c83fd9c2fb6f352f4
Matthieu Laqua <mlmatthieul@gmail.com>
2025-05-02 12:04:40 +0200

    chore(accessor): remove CombinedInventoryAccessor, add EquipmentAccessor

--- src/main/java/one/laqua/waig/mixin/EquipmentAccessor.java
+++ src/main/java/one/laqua/waig/mixin/EquipmentAccessor.java
@@ -1,17 +1,14 @@
 package one.laqua.waig.mixin;
 
+import net.minecraft.entity.EntityEquipment;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-import java.util.List;
-
 @Mixin(PlayerInventory.class)
-public interface CombinedInventoryAccessor {
+public interface EquipmentAccessor {
 
     @Accessor
-    List<DefaultedList<ItemStack>> getCombinedInventory();
+    EntityEquipment getEquipment();
 
 }
